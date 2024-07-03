3276da3266644051a86c7069afa24c11795c8ae4
Luiz Henrique <69602253+LuizHenriquedc@users.noreply.github.com>
2024-07-03 15:14:29 -0300

    Update TesteGitEclipse.java

--- IntegracaoGitEclipse/src/GitTeste/TesteGitEclipse.java
+++ IntegracaoGitEclipse/src/GitTeste/TesteGitEclipse.java
@@ -5,6 +5,7 @@ public class TesteGitEclipse {
 	public static void main(String[] args) {
 		
 		System.out.println("Integração Git Eclipse, primeiro teste");
+		System.out.println("Integração Git Eclipse, primeiro teste 2");
 
 	}
 
